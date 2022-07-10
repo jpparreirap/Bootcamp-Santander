b8d78828a546cca49f7e06ab836b3fcb3e1046df
jpparreirap <jpparreirap@gmail.com>
2022-07-10 00:13:03 -0300

    Desafios de codigo

---
+++ Spring/desafiosDeCodigo-Intermediario/src/dio/bootcamp/santander/q1/Idades.java
@@ -0,0 +1,27 @@
+package dio.bootcamp.santander.q1;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Idades {
+
+	public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int cont = 0;
+        int soma = 0;
+        int n = leitor.nextInt();
+        
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+        
+        while (n >= 1){
+            if (n >= 0) {
+                soma += n;
+                cont++;
+            }
+            n = leitor.nextInt();
+        }
+        double media = (double)soma/cont;
+        System.out.println(String.format("%.2f", media));
+    }
+
+}
