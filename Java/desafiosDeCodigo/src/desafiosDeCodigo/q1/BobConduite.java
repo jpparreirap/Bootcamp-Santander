78c3c8a79983547ed1e620160d4f572cc4f7b25b
jpparreirap <jpparreirap@gmail.com>
2022-07-09 23:54:08 -0300

    Desafios de Codigo - Santander

---
+++ Java/desafiosDeCodigo/src/desafiosDeCodigo/q1/BobConduite.java
@@ -0,0 +1,20 @@
+package desafiosDeCodigo.q1;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class BobConduite {
+
+	public static void main(String[] args) throws IOException {
+		Scanner scan = new Scanner(System.in);
+		int T = scan.nextInt();
+
+		for (int i = 0; i < T; i++) {
+			int R1 = scan.nextInt();
+			int R2 = scan.nextInt();
+			
+			System.out.println(R1+R2);
+		}
+	}
+
+}
---
+++ Java/desafiosDeCodigo/src/desafiosDeCodigo/q2/Pneu.java
@@ -0,0 +1,16 @@
+package desafiosDeCodigo.q2;
+
+import java.util.Scanner;
+
+public class Pneu {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int N = scan.nextInt();
+		int M = scan.nextInt();
+
+		System.out.println(N - M);
+	}
+	
+}
